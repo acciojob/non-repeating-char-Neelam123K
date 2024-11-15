af9940c40587a7fa56d0d7f1f3382a2afc5a17a3
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2024-11-15 15:11:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,22 +5,7 @@ function firstNonRepeatedChar(str) {
 		return null
 	}
 	 let freqObj = {}
-	for(let i = 0; i < str.length; i++)
-		{
-			if(!freqObj[str.charAt(i)])
-			{
-				freqObj[str.charAt(i)] = 1
-			}
-			else
-			{
-				freqObj[str[str.charAt(i)]] = freqObj[str.charAt(i)]+1
-			}
-		}
-	for(let item in freqObj)
-		{
-			if(freqObj[item] == 1)
-		}
-	return item
+	for(let)
 }
-// const input = prompt("Enter a string");
-// alert(firstNonRepeatedChar(input)); 
+const input = prompt("Enter a string");
+alert(firstNonRepeatedChar(input)); 
